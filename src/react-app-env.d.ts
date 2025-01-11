64d94ec343eb519f3a5ec1a986d812f545d870a1
mirzohidd <mirzohidmurodov77@gmail.com>
2025-01-12 03:59:39 +1000

    BRR: initial setup

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,30 @@
+// / <reference types="react-scripts" />
+declare module "*.module.css" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.png" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpeg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.gif" {
+  const content: string;
+  export default content;
+}
