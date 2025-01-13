447035edd9b678bc27b582d002a9a1e0b02e74f1
mirzohidd <mirzohidmurodov77@gmail.com>
2025-01-14 04:05:27 +1000

    feat and fix: develop burak  screen based components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,49 @@
 import React from "react";
-import logo from "./logo.svg";
 
 import "../css/App.css";
 import { Container, Stack, Box, Typography, Button } from "@mui/material";
-import { RippleBadge } from "./MaterialTheme/styled";
+import { Link, Route, Switch } from "react-router-dom";
+import { HomePage } from "./screens/homePage";
+import { ProductsPage } from "./screens/productsPage";
+import { OrdersPage } from "./screens/ordersPage";
+import { UserPage } from "./screens/userPage";
 function App() {
   return (
-    <Container maxWidth="sm">
-      <Stack flexDirection={"column"}>
-        <Box sx={{ my: 4 }}>
-          <Typography variant="h4" component={"h4"}>
-            Create React App on Typescript with Redux
-          </Typography>
-        </Box>
-        <Box>
-          <RippleBadge badgeContent="4">
-            <Button variant="contained">Contained</Button>
-          </RippleBadge>
-        </Box>
-      </Stack>
-    </Container>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home - Page</Link>
+          </li>
+          <li>
+            <Link to="/products">Products - Page</Link>
+          </li>
+          <li>
+            <Link to="/orders">Orders - Page</Link>
+          </li>
+          <li>
+            <Link to="/member-page">User- Page</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/products">
+          <ProductsPage></ProductsPage>
+        </Route>
+        <Route path="/orders">
+          <OrdersPage></OrdersPage>
+        </Route>
+        <Route path="/user">
+          <UserPage></UserPage>
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
@@ -8,19 +9,22 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./app/MaterialTheme";
 import "./css/index.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
-const container = document.getElementById("root")!;
-const root = createRoot(container);
 
-root.render(
+
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <App />
+        <Router>
+          <App />
+        </Router>
       </ThemeProvider>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
