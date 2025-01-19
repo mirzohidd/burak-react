407aff40d3e7e2865761d439613ba036dbc7c0d2
mirzohidd <mirzohidmurodov77@gmail.com>
2025-01-19 17:28:54 +1000

    feat:develop advertisment component

--- src/app/screens/homePage/Advertisement.tsx
+++ src/app/screens/homePage/Advertisement.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 export default function Advertisement() {
   return (
-    <div>
-        Advertisement
+    <div className="ads-restaurant-frame">
+       <video className={"ads-video"} autoPlay={true} loop muted playsInline data-video-media="">
+        <source type="video/mp4" src="video/burak-ads.mp4"/>
+       </video>
     </div>
   );
 }
